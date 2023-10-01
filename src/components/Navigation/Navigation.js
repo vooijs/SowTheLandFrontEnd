1dc5e79441d7b6406bea427fafb73d3ebc160082
vooijs <klaasvooijs@gmail.com>
2023-10-01 21:49:14 +0200

    editted hamburger menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Navigation.css';
 import logo from "../../assets/logoSowTheLand.jpg";
+import hamburger from "../../assets/hamburger.png";
 import {NavLink} from "react-router-dom";
 
 function Navigation() {
@@ -8,34 +9,53 @@ function Navigation() {
     return(
         <div className="header">
             <div>
-                <img src={logo} alt="logo" className="logo"/>
+                <a href="/" className="logo-container">
+                    <img src={logo} alt="logo" className="logo"/>
+                </a>
             </div>
 
-            <button className="dropdown-menu">hamburger menu</button>
+            <div class="dropdown">
+                <button className="dropbtn">
+                    <img src={hamburger} alt="hamburger" className="hamburger" />
+                </button>
+                <div className="dropdown-content">
+                    <a href="/login">Login</a>
+                    <a href="/home">Home</a>
+                    <a href="/garden">Garden</a>
+                    <a href="/crops">Crops</a>
+                    <a href="/blogs">Blogs</a>
+                </div>
+            </div>
 
             <ul className="nav">
                 <li>
                     <NavLink to="/login" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>
-                        Register/Log-in
-                    </NavLink></li>
+                        Login
+                    </NavLink>
+                </li>
                 <li>
                     <NavLink to="/home" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>
-                        Home</NavLink>
+                        Home
+                    </NavLink>
                 </li>
                 <li>
                     <NavLink to="/garden" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>
-                        Garden</NavLink>
+                        Garden
+                    </NavLink>
                 </li>
                 <li>
                     <NavLink to="/crops" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>
-                        Crops</NavLink>
+                        Crops
+                    </NavLink>
                 </li>
                 <li>
                     <NavLink to="/blogs" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>
-                        Blogs</NavLink>
+                        Blogs
+                    </NavLink>
                 </li>
             </ul>
         </div>
     );
 }
+
 export default Navigation;
