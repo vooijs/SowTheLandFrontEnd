a440d62e33e86eeaaa98f66be1eb10256753ca41
vooijs <klaasvooijs@gmail.com>
2023-10-07 08:40:22 +0200

    styled homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
-
-import devices from "../../assets/promo.png"
-import apple from "../../assets/applePic.webp"
+import "./Home.css";
+import devices from "../../assets/promo.png";
+import apple from "../../assets/applePic.webp";
+import cherry from "../../assets/cherry.jpg";
+import peach from "../../assets/peach.jpg";
 import Button from "../../components/button/Button";
 import Blog from "../../components/Blog/Blog";
 
@@ -16,7 +18,6 @@ function Home () {
                        <button>Search</button>
                    </form>
                </div>
-
            </div>
            <div className="section">
                <div className= "seasonSection">
@@ -27,9 +28,11 @@ function Home () {
                </div>
            </div>
            <div className="section">
-               <div className="blogpost">
-                   <img src={devices} alt="devices" className="devicesPic"/>
-                   <article className="blog">
+               <div className="textAndImageContainer">
+                    <div className="devicesPicContainer">
+                        <img src={devices} alt="devices" className="devicesPic"/>
+                    </div>
+                   <article className="textAndImage">
                        <h2>Sow The Land</h2>
                        <h4>The app that will help you grow your food</h4>
                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda repellat, sint. Assumenda aut deserunt dicta dignissimos dolore eligendi, esse, expedita impedit incidunt iusto porro quia reprehenderit soluta tempore vero voluptas.</p>
@@ -39,15 +42,32 @@ function Home () {
                </div>
            </div>
            <div className="section">
-               <Blog
-                   img={apple}
-                   alt="apple"
-                   title="apple"
-                   teaser="sweet and juicy apple"
-                   blog="lorem"
-               />
+            <div className="blogContainer">
+                <Blog
+                    img={apple}
+                    alt="apple"
+                    title="Apple"
+                    teaser="Sweet and juicy apple"
+                    blog="lorem"
+                />
+                <Blog
+                    img={cherry}
+                    alt="cherry"
+                    title="Cherry"
+                    teaser="Pop my cherry"
+                    blog="lorem"
+                />
+                <Blog
+                    img={peach}
+                    alt="peach"
+                    title="Peach"
+                    teaser="I can eat a peach for hours"
+                    blog="lorem"
+                />
+                </div>
            </div>
        </div>
-    );
+    )
 }
-export default Home;
+
+export default Home
